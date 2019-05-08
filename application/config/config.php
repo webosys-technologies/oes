6bce77bc9a8de091b7ed1cb1b1f791000d5614cf
Sunil Khandare <sunilkhandare88@gmail.com>
2019-05-08 21:42:24 +0530

    https
    https

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
- $base  = "http://".$_SERVER['HTTP_HOST'];
+ $base  = "https://".$_SERVER['HTTP_HOST'];
 $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 $config['base_url'] = $base;
